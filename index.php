78a87429dc8b40678b7e4e5b50f76ec9bdb639d4
pan8966 <p.laurelle@gmail.com>
2018-05-10 00:34:45 -0400

    first website upload

---
+++ index.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CogniDoodle</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap-4.0.0.css" rel="stylesheet">
+	<link href="css/cogni.css" rel="stylesheet" type="text/css">
+	<link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" rel="stylesheet">
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+  </head>
+  <body>
+  <div class="row d-flex d-md-block flex-nowrap wrapper">
+      <!--NAVBAR-->
+      <div class="col-md-3 float-left col-1 pl-0 pr-0 collapse width show" id="sidebar">
+        <div class="logo-link w-nav-brand" href="#">
+          <span style="font-weight: 900;color: #ff6600">
+            cogni</span><span style="font-weight: 300;color: #ffaa00">doodle</span>
+        </div>
+        <div class="list-group border-0 card text-center text-md-left">
+          <a class="list-group-item d-inline-block collapsed" href="#home">
+            <i class="fa fa-home">
+            </i>
+            <span class="d-none d-md-inline">
+              Home
+            </span>
+          </a>
+          <a class="list-group-item d-inline-block collapsed" href="#featurettes2">
+            <i class="fas fa-people-carry">
+            </i>
+            <span class="d-none d-md-inline">
+              Mission
+            </span>
+          </a>
+          <a class="list-group-item d-inline-block collapsed" data-parent="#sidebar" href="#founders-us">
+            <i class="fab fa-slideshare">
+			  </i>
+            <span class="d-none d-md-inline">
+              Our Founders
+            </span>
+          </a>
+          <a class="list-group-item d-inline-block collapsed" data-parent="#sidebar" href="#gallery">
+            <i class="far fa-images">
+            </i>
+            <span class="d-none d-md-inline">
+              Gallery
+            </span>
+          </a>
+          <a class="list-group-item d-inline-block collapsed" data-parent="#sidebar" href="#info">
+            <i class="far fa-star">
+            </i>
+            <span class="d-none d-md-inline">
+              Classes
+            </span>
+          </a>
+          <a class="list-group-item d-inline-block collapsed" data-parent="#sidebar" href="#contact">
+            <i class="far fa-envelope">
+            </i>
+            <span class="d-none d-md-inline">
+              Contact Us
+            </span>
+          </a>
+        </div>
+      </div>
+      <!--END OF NAVBAR-->
+      <main class="col-md-9 float-left col px-5 pl-md-2 main">
+        <a data-target="#sidebar" data-toggle="collapse" href="#">
+          <i class="fa fa-navicon fa-2x py-2 p-1 float-left">
+          </i>
+        </a>
+<div class="section banner" id="home">
+  <div class="hero-title-wrapper">
+    <img alt="" src="https://farm1.staticflickr.com/801/39256196390_3fe9a3cf80_o.png" style="width:75%"/>
+    <div class="hero-subheading">
+      <h4 class="hero-heading">
+        Learning through art
+      </h4>
+    </div>
+    <div>
+      <a class="hollow-button white" href="#gallery">
+        Gallery
+      </a>
+      <a class="hollow-button red" href="#contact">
+        Get in Touch
+      </a>
+    </div>
+    <div class="hero-welcome">
+      We are art enthusiasts and providers of art enrichment to children with special needs in Fulton, Cobb, and Paulding Counties.
+      <br/>
+      We believe everyone has an artist inside looking to express themselves,<br/> and we want to help your child find their artistic voice.
+    </div>
+  </div>
+        </div>
+        <section class="section" id="featurettes2">
+          <div class="welcome-banner" id="welcome">
+            <div class="why-us">
+              <p>
+                At CogniDoodle it is our <em>mission</em> to offer a service that many children with special needs do not have the opportunity to try.&#160; We strive to enhance and strengthen cognitive and language skills while students learn new ways to express themselves.
+                <br/><br/>
+Our primary focus is on process-based art enrichment where the value of creating art is the experience, not the finished product.  This allows students to have full creative freedom.
+                <br/><br/>
+Everyone on our staff has experience working with children with exceptionalities.  We offer art enrichment classes and social skill groups for children through our visual arts and language-based curriculum.  Visual art experiences support children by growing skills such as critical thinking, self-expression, problem-solving, communication, and collaboration.
+              </p>
+             
+            </div>
+          </div>
+        </section>
+        <section class="section founders">
+          <div class="row" id="founders-us">
+            <div class="col-md-12">
+              <h2 class="featurette-heading">
+				  <i class="fab fa-slideshare"></i>
+                ABOUT OUR FOUNDERS
+              </h2>
+              <p>
+                Our founders Tristyn and Laurelle are proud mothers to loving, caring, talented, and creative kids.  They each have a child who was made 
+                <b>
+                  different, not less
+                </b>
+                .  Fusing each founders&#39; education and passion, CogniDoodle aims to offer more programming for families with special needs children.
+              </p>
+            </div>
+          </div>
+          <div class="row justify-content-center" id="about-us">
+            <div class="col-md-5 tristyn">
+              <p>
+                Tristyn has  9 years of experience providing support in schools and clinical settings.  She has a background in speech and language pathology.  She is currently pursuing her Master&#39;s in Counseling.  In these settings, she served as a member of the Assistive Technology Committee and Coordinator for her school&#8217;s  Autism Support Team. Tristyn spends her free time baking with her son, gardening, crafting, and advocating for families with special needs.
+              </p>
+            </div>
+            <div class="col-md-5 laurelle offset-md-1">
+              <p>
+                Laurelle is a professional illustrator and designer.  She has 7 years of experience as an art instructor to seniors and children.  She has a background in Science and Fine Arts.  She also has experience working in elementary schools teaching Art, STEM, and Robotics.  Her specialties include colored pencil and acrylic media.  Laurelle enjoys coloring with her children, working as a makeup artist, painting, crocheting, and volunteering as a Girl Scout troop co-leader.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="section" id="gallery">
+          <div class="row action">
+            <div class="col">
+              <h2 class="call-to-action-heading">
+                Photo gallery coming soon!
+              </h2>
+            </div>
+            <div class="align-right">
+              <a class="hollow-button white" href="#contact">
+                Get in Touch
+              </a>
+            </div>
+          </div>
+        </section>
+        <section class="section" id="featurettes3">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>
+                TAKE A CLASS
+              </h3>
+            </div>
+          </div>
+        </section>
+        <section class="section" id="info">
+          <div class="row">
+            <div class="col-md-10 offset-md-1">
+              <div class="row">
+                <div class="col-md-4">
+                  <img alt="" height="60" src="https://farm1.staticflickr.com/896/39256233250_68a165c76f_o.png" width="60"/>
+                  <br/>
+                  <br/>
+                  <h4>
+                    PEER
+                    <br/>
+                    CONNECT
+                  </h4>
+                  <p>
+                    Practice social skills.
+                  </p>
+                 
+                    <button aria-expanded="false" class="button" data-target="#demo1" data-toggle="collapse" role="button">
+                    View details 
+                    <i class="fa fa-angle-double-down">
+                    </i>
+                  </button>
+                  <div aria-expanded="false" class="height collapse" id="demo1">
+                    <div class="star-info">
+                      <p>
+                        Here your child will learn new skills and make new friends.  Using the CogniDoodle curriculum, our instructors focus on building social interaction and communication skills.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.col-md-4 -->
+                <div class="col-md-4">
+                  <img alt="" height="60" src="https://farm1.staticflickr.com/896/39256233250_68a165c76f_o.png" width="60"/>
+                  <br/>
+                  <br/>
+                  <h4>
+                    INDIVIDUAL
+                    <br/>
+                    LESSONS
+                  </h4>
+                  <p>
+                    Awaken your inner artist.
+                  </p>
+                 
+                    <button aria-expanded="false" class="button" data-target="#demo3" data-toggle="collapse" role="button">
+                    View details 
+                    <i class="fa fa-angle-double-down">
+                    </i>
+                  </button>
+              <div aria-expanded="false" class="height collapse" id="demo3">
+                    <div class="star-info">
+                      <p>
+                       Not quite ready to join a group? Or is your child an aspiring artist?  We will tailor the lessons to meet personal needs and interests of the student.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.col-md-4 -->
+                <div class="col-md-4">
+                  <img alt="" height="60" src="https://farm1.staticflickr.com/896/39256233250_68a165c76f_o.png" width="60"/>
+                  <br/>
+                  <br/>
+                  <h4>
+                    HOST
+                    <br/>
+                    COGNIDOODLE
+                  </h4>
+                  <p>
+                    Invite us to teach.
+                  </p>
+                  <button aria-expanded="false" class="button" data-target="#demo2" data-toggle="collapse" role="button">
+                    View details 
+                    <i class="fa fa-angle-double-down">
+                    </i>
+                  </button>
+                  <div aria-expanded="false" class="height collapse" id="demo2">
+                    <div class="star-info">
+                      <p>
+                        We bring CogniDoodle classes to your location. We provide our CogniDoodle curriculum, a variety of creative materials, and a CogniDoodle trained teacher. You provide adequate space for us to offer the classes.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.col-md-4 -->
+              </div>
+            </div>
+            <!-- /.row -->
+          </div>
+        </section>
+        <!--END OF COLUMNS-->
+        <!--DO NOT DELETE BELOW-->
+        <!-- CONTACT FORM -->                         
+<section class="section" id="contact">
+
+    <!--Section heading-->
+    <h2 class="section-heading h1 pt-4">Get in touch.</h2>
+    <!--Section description-->
+    <p class="section-description">Do you have any questions? Please do not hesitate to contact us directly.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-8 col-xl-9">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">Your name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form">
+                            <input type="text" id="email" name="email" class="form-control">
+                            <label for="email" class="">Your email</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+			
+			<div class="g-recaptcha" data-sitekey="6LcXZFgUAAAAAOw_KYMS8nR7IDdQMWnKwELNabcJ"></div>
+
+            <div class="center-on-small-only">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-4 col-xl-3">
+            <ul class="contact-icons">
+                <li><i class="fa fa-map-marker fa-2x"></i>
+                  <p>Atlanta, GA, USA</p>
+                </li>
+
+                <li><i class="fa fa-phone fa-2x"></i>
+                    <p>(123) 456-789</p>
+                </li>
+
+                <li><i class="fa fa-envelope fa-2x"></i>
+                    <p>contact@cognidoodle.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
+</section>
+<!--Section: Contact v.2-->
+
+        <!-- /END CONTACT FORM -->
+        <!-- FOOTER -->
+        <footer>
+          <p class="float-right">
+            <a href="#home">
+              Back to top
+            </a>
+          </p>
+          <p>
+            &#169; 2017-2018 CogniDoodle, LLC.
+          </p>
+        </footer>
+        <!-- /END FOOTER -->
+      </main>
+    </div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-3.2.1.min.js"></script>
+	      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="validator.js"></script>
+  <script src="contact.js"></script>
+    <script type="text/javascript" src="js/popper.min.js"></script>
+	   <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script src="js/bootstrap-4.0.0.js"></script>
+	  <script type="text/javascript" src="js/mdb.js"></script>
+	      <!--Custom scripts-->
+    <script>
+
+function validateForm() {
+    // var x =  document.getElementById('name').value;
+    // if (x == "") {
+    //     document.getElementById('status').innerHTML = "Name cannot be empty";
+    //     return false;
+    // }
+    // x =  document.getElementById('email').value;
+    // if (x == "") {
+    //     document.getElementById('status').innerHTML = "Email cannot be empty";
+    //     return false;
+    // } else {
+    //     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     if(!re.test(x)){
+    //         document.getElementById('status').innerHTML = "Email format invalid";
+    //         return false;
+    //     }
+    // }
+    // x =  document.getElementById('subject').value;
+    // if (x == "") {
+    //     document.getElementById('status').innerHTML = "Subject cannot be empty";
+    //     return false;
+    // }
+    // x =  document.getElementById('message').value;
+    // if (x == "") {
+    //     document.getElementById('status').innerHTML = "Message cannot be empty";
+    //     return false;
+    // }
+ //get input field values data to be sent to server
+    document.getElementById('status').innerHTML = "Sending...";
+    formData = {
+        'name'     : $('input[name=name]').val(),
+        'email'    : $('input[name=email]').val(),
+        'subject'  : $('input[name=subject]').val(),
+        'message'  : $('textarea[name=message]').val()
+    };
+
+
+   $.ajax({
+    url : "mail.php",
+    type: "POST",
+    data : formData,
+    success: function(data, textStatus, jqXHR)
+    {
+
+        $('#status').text(data.message);
+        if (data.code) //If mail was sent successfully, reset the form.
+        $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+    },
+    error: function (jqXHR, textStatus, errorThrown)
+    {
+        $('#status').text(jqXHR);
+    }
+});
+
+
+
+}
+</script>
+	</body>
+</html>
